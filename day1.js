8fd729c14abf2a3bd5bd1e53704795a56c01a4a2
Zalic <zalicross24@gmail.com>
2025-06-12 17:52:23 -0500

    day1

---
+++ day1.js
@@ -0,0 +1,34 @@
+const userName = "John"
+
+let admin = userName; 
+
+alert(admin);
+
+const ourPlanetName = "Earth"; 
+
+let currentUserName = "Eric";
+
+
+
+let name = "Ilya";
+
+alert( `hello ${1}` ); // hello 1 
+
+alert( `hello ${"name"}` ); // hello name
+
+alert( `hello ${name}` ); // hello Ilya 
+
+
+
+// Pratice 
+
+const myName = "Zee";
+
+const myAge = 31; 
+
+isLearning = true; 
+
+alert(`My name is ${myName} and I am ${myAge} years old. I am trying to gain a deeper understanding of JavaScript: ${isLearning}`);
+
+
+
